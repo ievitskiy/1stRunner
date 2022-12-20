9f44366e6256828c16419b395347b00009d85fa0
Konstantin Zaitsev <wrsss0040@gmail.com>
2022-12-20 19:20:03 +0600

    upd

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] KeyCode up;
+    [SerializeField] KeyCode down;
+    [SerializeField] KeyCode right;
+    [SerializeField] KeyCode left;
+    [SerializeField] KeyCode jump;
+    
+    [SerializeField] int speed;
+    [SerializeField] int jumpforce;
+
+    bool cubeIsOnGround = true;
+
+    public GameManager gm;
+
+
+    private Rigidbody cube;
+    void Awake()
+    {
+        cube = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(up))
+            cube.AddForce(speed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        if (Input.GetKey(down))
+            cube.AddForce(-speed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        if (Input.GetKey(right))
+            cube.AddForce(0, 0, -speed * Time.deltaTime, ForceMode.VelocityChange);
+        if (Input.GetKey(left))
+            cube.AddForce(0, 0, speed * Time.deltaTime, ForceMode.VelocityChange);
+
+        if (Input.GetKey(jump) && cubeIsOnGround)
+        {
+            cube.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            cubeIsOnGround = false;
+        }
+
+
+        if (transform.position.y < -10)
+            gm.EndGame();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ground")
+            cubeIsOnGround = true;
+
+        if (collision.collider.tag == "wall")
+        {
+            gm.EndGame();
+        }
+    }
+}
